4882620f0ce1709f76dd13f9915fb7ce41900166
Manivardhan <159030573+Manivardhan-10k@users.noreply.github.com>
2026-07-21 09:56:46 +0530

    Delete file.py

--- file.py
+++
@@ -1,8 +0,0 @@
-user_name="manivardhan"
-user_password="secret_123"
-
-def login(name,password):
-    if user_name==name and user_password==password:
-           print("login successful")
-    else:
-          print("invalid credentials")
